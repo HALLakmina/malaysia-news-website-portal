2ccb1a490041dd1e1df5fd6765626d69388a9d37
HALLakmina <lahirulakmina1999@gmail.com>
2024-07-24 23:37:10 +0530

    Link the new changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import NavigationBar from './Components/NavigationBar'
+import Index from "./Pages/Index"
 const App = () =>{
   return (
     <div className="main-component-wrapper color-main-bg">
