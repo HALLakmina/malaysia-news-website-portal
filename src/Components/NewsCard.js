9303df1a207625b088513b1951f75672834c5768
HALLakmina <lahirulakmina1999@gmail.com>
2024-07-24 23:43:09 +0530

    Rebase the new changes in dev

--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 const NewsCard = () => {
   return (
-    <div className="rounded-lg p-3 color-bg-white flex flex-col" style={{width:'380px'}}>
+    <div className="rounded-lg p-3 color-bg-white flex flex-col relative" style={{width:'380px'}}>
+        <div className="absolute top-0 right-0">Latest</div>
         <div style={{width:'100%',height:'200px'}}>
             <img src="" alt="" className="size-full rounded-lg"/>
         </div>
         <p className="heading-font-5">UNESCO Director-General in SL</p>
-        <p className="body-font-5 color-text-on-ash-gray">July 16, 2024  4:32 pm</p>
+        <p className="body-font-5 color-text-on-ash-gray">July 16, 2024</p>
         <p className="body-font-5">
             Audrey Azoulay, the Director-General of the United Nations Educational, 
             Scientific and Cultural Organization (UNESCO), participated in the celebration 
@@ -15,7 +16,7 @@ const NewsCard = () => {
             Theatre today Pix by Waruna Wanniarachchi
         </p>
         <div className='flex flex-row items-center justify-center my-5' style={{width:'100%'}}>
-            <button className="body-font-5 font-bold">READ MORE</button>
+            <button className="body-font-5 font-bold" aria-label="Read More Button" type="button">READ MORE</button>
         </div>
     </div>
   )
