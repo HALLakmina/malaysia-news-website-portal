c678009697d1e4f4cbd39eec5d6db8caf4bd1099
HALLakmina <lahirulakmina1999@gmail.com>
2024-08-03 19:43:09 +0530

    Create the about us page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import NavigationBar from './Components/NavigationBar/NavigationBar'
 import Index from "./Pages/Index"
 import NewsPage from "./Pages/NewsPage";
 import NewsReadPage from "./Pages/NewsReadPage";
+import AboutUs from "./Pages/AboutUs";
 
 const App = () =>{
   return (
     <div className="main-component-wrapper color-main-bg">
       <div className="main-component-body">
         <NavigationBar/>
-        <Routes>
-          <Route  path="/" element={<Index/>}/>
-          <Route  path="/news/:type" element={<NewsPage/>}/>
-          <Route  path="/news/:type/:id" element={<NewsReadPage/>}/>
-          <Route  path="/about-us" element={''}/>
-          <Route  path="/contact-us" element={''}/>
-        </Routes>
+        <div className="min-h-screen w-full flex justify-center">
+          <Routes>
+            <Route  path="/" element={<Index/>}/>
+            <Route  path="/news/:type" element={<NewsPage/>}/>
+            <Route  path="/news/:type/:id" element={<NewsReadPage/>}/>
+            <Route  path="/about-us" element={<AboutUs/>}/>
+            <Route  path="/contact-us" element={''}/>
+          </Routes>
+        </div>
       </div>
     </div>
   );
